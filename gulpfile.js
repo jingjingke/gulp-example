85cda866def1876aa4907d4f5d9ea9bcc711f951
cuiziyan <919948650@qq.com>
2018-09-13 18:54:18 +0800

    初始化-html压缩

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin');
+
+gulp.task('html', function () {
+    return gulp.src('src/html/*.html')
+        .pipe(htmlmin({
+            removeComments: true,                //清除HTML注释
+            collapseWhitespace: true,            //压缩HTML
+            collapseBooleanAttributes: true,     //省略布尔属性的值 <input checked="true"/> ==> <input />
+            removeEmptyAttributes: true,         //删除所有空格作属性值 <input id="" /> ==> <input />
+            removeScriptTypeAttributes: true,    //删除<script>的type="text/javascript"
+            removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+            minifyJS: true,                      //压缩页面JS
+            minifyCSS: true                      //压缩页面CSS
+        }))
+        .pipe(gulp.dest('dist/html'));
+});
